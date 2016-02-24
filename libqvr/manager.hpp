1584290f92db1ad8150025f366050a0cfa91524f
Martin Lambers <marlam@marlam.de>
2016-02-24 12:29:54 +0100

    Document QVRManager.

--- libqvr/manager.hpp
+++ libqvr/manager.hpp
@@ -44,14 +44,50 @@ class QVRWindow;
 class QVRProcess;
 class QVRRenderContext;
 
+/*!
+ * \brief The level of logging of the QVR framework
+ *
+ * You can use the command line or an environment variable to set
+ * the log level:
+ * \code{.unparsed}
+ * <application> --qvr-log-level=fatal|warning|info|debug|firehose
+ * export QVR_LOG_LEVEL=FATAL|WARNING|INFO|DEBUG|FIREHOSE
+ * \endcode
+ */
 typedef enum {
-    QVR_Log_Level_Fatal = 0,    // print only fatal errors
-    QVR_Log_Level_Warning = 1,  // additionally print warnings (default)
-    QVR_Log_Level_Info = 2,     // additionally print informational messages
-    QVR_Log_Level_Debug = 3,    // additionally print debugging information
-    QVR_Log_Level_Firehose = 4  // additionally print verbose per-frame debugging information
-} QVRLogLevel; // you can set environment variable QVR_LOG_LEVEL=FATAL|WARN|INFO|DEBUG|FIREHOSE
+    /*! Print only fatal errors */
+    QVR_Log_Level_Fatal = 0,
+    /*! Additionally print warnings (default) */
+    QVR_Log_Level_Warning = 1,
+    /*! Additionally print informational messages */
+    QVR_Log_Level_Info = 2,
+    /*! Additionally print debugging information */
+    QVR_Log_Level_Debug = 3,
+    /*! Additionally print verbose per-frame debugging information */
+    QVR_Log_Level_Firehose = 4
+} QVRLogLevel;
 
+/*!
+ * \brief This class manages the QVR application's control flow and its processes and windows.
+ *
+ * The QVRManager object is typically created right after the QApplication object.
+ * Afterwards, the application typically sets its preferred OpenGL context properties,
+ * and then initializes the QVRManager object:
+ * \code{.cpp}
+ * QApplication app(argc, argv);
+ * QVRManager manager(argc, argv);
+ * // Set OpenGL context properties:
+ * QSurfaceFormat format;
+ * format.setProfile(QSurfaceFormat::CoreProfile);
+ * format.setVersion(3, 3);
+ * QSurfaceFormat::setDefaultFormat(format);
+ * // Start the QVR application
+ * MyQVRApp myqvrapp;
+ * if (!manager.init(&myqvrapp))
+ *     return 1;
+ * return app.exec();
+ * \endcode
+ */
 class QVRManager : public QObject
 {
 Q_OBJECT
@@ -101,21 +137,82 @@ private slots:
     void printFps();
 
 public:
+    /*!
+     * \brief This constructor creates the manager object.
+     *
+     * The following command line options are intereted by the QVR manager
+     * and removed from \a argc and \a argv:
+     * - --qvr-config=\<config.qvr\>
+     *   Specify a QVR configuration file.
+     * - --qvr-log-level=\<level\>
+     *   See \a QVRLogLevel.
+     * - --qvr-fps=\<n\>
+     *   Make QVR report frames per second measurements every n milliseconds.
+     */
     QVRManager(int& argc, char* argv[]);
+
+    /*! \brief Destructor. */
     ~QVRManager();
 
+    /*!
+     * \brief Initialize the QVR application.
+     * \param app       The QVR application.
+     * \return  False on failure.
+     *
+     * This function will create all slave processes and all windows, depending
+     * on the QVR configuration, and it will call the initialization functions
+     * of \a app.
+     */
     bool init(QVRApp* app);
 
+    /*!
+     * \brief Return the QVR manager instance.
+     *
+     * There can be only one instance of \a QVRManager. This function returns it.
+     */
     static QVRManager* instance();
+
+    /*!
+     * \brief Return the log level.
+     */
     static QVRLogLevel logLevel();
 
+    /*!
+     * \brief Return the index of this \a QVRProcess in the active configuration.
+     *
+     * See \a config().
+     */
     static int processIndex();
 
+    /*!
+     * \brief Return the active configuration.
+     */
     static const QVRConfig& config();
+
+    /*!
+     * \brief Return the configuration of the observer with index \a observerIndex.
+     *
+     * This is a convenience function, you can also get this information from \a config().
+     */
     static const QVRObserverConfig& observerConfig(int observerIndex);
+
+    /*!
+     * \brief Return the configuration of the process with the index \a processIndex.
+     *
+     * This is a convenience function, you can also get this information from \a config().
+     */
     static const QVRProcessConfig& processConfig(int processIndex = processIndex());
+
+    /*!
+     * \brief Return the configuration of the window with the index \a windowIndex on
+     * the process with index \a processIndex.
+     *
+     * This is a convenience function, you can also get this information from \a config().
+     */
     static const QVRWindowConfig& windowConfig(int processIndex, int windowIndex);
 
+    /*! \cond
+     * These functions are only used internally. */
     static void enqueueKeyPressEvent(const QVRRenderContext& c, QKeyEvent* event);
     static void enqueueKeyReleaseEvent(const QVRRenderContext& c, QKeyEvent* event);
     static void enqueueMouseMoveEvent(const QVRRenderContext& c, QMouseEvent* event);
@@ -123,6 +220,7 @@ public:
     static void enqueueMouseReleaseEvent(const QVRRenderContext& c, QMouseEvent* event);
     static void enqueueMouseDoubleClickEvent(const QVRRenderContext& c, QMouseEvent* event);
     static void enqueueWheelEvent(const QVRRenderContext& c, QWheelEvent* event);
+    /*! \endcond */
 };
 
 #endif
