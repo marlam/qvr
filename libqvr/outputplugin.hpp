0f904f6f037f2876274f4970f5595fdcf318bd17
Martin Lambers <marlam@marlam.de>
2016-02-25 09:42:35 +0100

    Document the output plugin API.

--- libqvr/outputplugin.hpp
+++ libqvr/outputplugin.hpp
@@ -28,8 +28,40 @@ class QStringList;
 class QVRWindow;
 class QVRRenderContext;
 
+/*!
+ * \brief Initialize an output plugin.
+ * @param window        The window that provides the output
+ * @param args          Plugin argument list from the configuration file
+ *
+ * See also \a QVROutputPluginExit() and \a QVROutputPlugin().
+ */
 extern "C" bool QVROutputPluginInit(QVRWindow* window, const QStringList& args);
+
+/*!
+ * \brief Cleanup an output plugin.
+ * @param window        The window that provides the output
+ *
+ * See also \a QVROutputPluginInit() and \a QVROutputPlugin().
+ */
 extern "C" void QVROutputPluginExit(QVRWindow* window);
+
+/*!
+ * \brief Output the rendered content.
+ * @param window        The window that provides the output
+ * @param context       The render context of the output
+ * @param tex0          The output of view pass 0
+ * @param tex1          The output of view pass 1 (if any)
+ *
+ * This is the main function of an output plugin. It takes the rendered output
+ * from the textures \a tex0 and \a tex1 and displays it to the output device,
+ * applying arbitrary postprocessing.
+ *
+ * The \a context provides all information available about the output. Note that
+ * \a tex1 is only valid if the output mode of \a window has two view passes.
+ * This can be checked with \a QVRRenderContex::viewPasses().
+ *
+ * See also \a QVROutputPluginInit() and \a QVROutputPluginExit().
+ */
 extern "C" void QVROutputPlugin(QVRWindow* window,
         const QVRRenderContext& context,
         unsigned int tex0, unsigned int tex1);
