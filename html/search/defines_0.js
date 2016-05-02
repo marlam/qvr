b5115a36a9df7257e9eb8fe1065aa89642b21141
Martin Lambers <marlam@marlam.de>
2016-05-02 09:46:56 +0200

    Update to 0.9.4.

--- html/search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['qvq_5fnavigator_5fhpp',['QVQ_NAVIGATOR_HPP',['../navigator_8hpp.html#a09f74955e457075801fea1c800d4dfbe',1,'navigator.hpp']]]
-];
--- html/search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['initialeyedistance',['initialEyeDistance',['../config_8hpp.html#a400549974abbfdbfc8eb123d7a5dcb72',1,'config.hpp']]]
-];
