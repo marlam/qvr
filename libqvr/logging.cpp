9b968a9a0d2482db3ea12d6f183370811a39f4ec
Martin Lambers <marlam@marlam.de>
2016-10-10 14:57:39 +0200

    Print process index in log messages only if multiple processes are present.
    This was always the intention; a stupid type prevented it.

--- libqvr/logging.cpp
+++ libqvr/logging.cpp
@@ -36,7 +36,7 @@ void QVRMsg(const char* s)
     // mangled. Therefore we buffer what we want to print.
     char buf[QVR_MSG_BUFSIZE] = "QVR";
     int bufIndex = 3;
-    if (QVRManager::config().processConfigs().size() > 0)
+    if (QVRManager::config().processConfigs().size() > 1)
         bufIndex += snprintf(buf + bufIndex, QVR_MSG_BUFSIZE - bufIndex, "[%d]", QVRManager::processIndex());
     bufIndex += snprintf(buf + bufIndex, QVR_MSG_BUFSIZE - bufIndex, ": %s", s);
     buf[std::min(bufIndex, QVR_MSG_BUFSIZE - 2)] = '\n';
