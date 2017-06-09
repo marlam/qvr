93de01b2efe145cd1f19cdc210d48f27a90f404e
Martin Lambers <marlam@marlam.de>
2017-06-09 10:14:41 +0200

    Use lowp precision for colors on OpenGL ES

--- libqvr/output-fs.glsl
+++ libqvr/output-fs.glsl
@@ -39,37 +39,37 @@ smooth in vec2 vtexcoord;
 layout(location = 0) out vec4 fcolor;
 
 // Source of this matrix: http://www.site.uottawa.ca/~edubois/anaglyph/LeastSquaresHowToPhotoshop.pdf
-const mat3 dubois_red_cyan_m0 = mat3(
+const lowp mat3 dubois_red_cyan_m0 = mat3(
         0.437, -0.062, -0.048,
         0.449, -0.062, -0.050,
         0.164, -0.024, -0.017);
-const mat3 dubois_red_cyan_m1 = mat3(
+const lowp mat3 dubois_red_cyan_m1 = mat3(
         -0.011, 0.377, -0.026,
         -0.032, 0.761, -0.093,
         -0.007, 0.009, 1.234);
 // Source of this matrix: http://www.flickr.com/photos/e_dubois/5132528166/
-const mat3 dubois_green_magenta_m0 = mat3(
+const lowp mat3 dubois_green_magenta_m0 = mat3(
         -0.062, 0.284, -0.015,
         -0.158, 0.668, -0.027,
         -0.039, 0.143, 0.021);
-const mat3 dubois_green_magenta_m1 = mat3(
+const lowp mat3 dubois_green_magenta_m1 = mat3(
         0.529, -0.016, 0.009,
         0.705, -0.015, 0.075,
         0.024, -0.065, 0.937);
 // Source of this matrix: http://www.flickr.com/photos/e_dubois/5230654930/
-const mat3 dubois_amber_blue_m0 = mat3(
+const lowp mat3 dubois_amber_blue_m0 = mat3(
         1.062, -0.026, -0.038,
         -0.205, 0.908, -0.173,
         0.299, 0.068, 0.022);
-const mat3 dubois_amber_blue_m1 = mat3(
+const lowp mat3 dubois_amber_blue_m1 = mat3(
         -0.016, 0.006, 0.094,
         -0.123, 0.062, 0.185,
         -0.017, -0.017, 0.911);
 
 void main(void)
 {
-    vec3 l, r;
-    vec3 color = vec3(0.8, 0.4, 0.2);
+    lowp vec3 l, r;
+    lowp vec3 color = vec3(0.8, 0.4, 0.2);
     switch (output_mode) {
     case QVR_Output_Stereo_Red_Cyan:
     case QVR_Output_Stereo_Green_Magenta:
