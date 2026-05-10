e472887c10cb398131d8e7eae34e070e92173d9a
Martin Lambers <marlam@marlam.de>
2026-05-10 10:07:45 +0200

    Do not ignore return value of QFile::open()

--- libqvr/window.cpp
+++ libqvr/window.cpp
@@ -136,13 +136,17 @@ void QVRWindowThread::run()
     _window->winContext()->moveToThread(QCoreApplication::instance()->thread());
 }
 
-// Helper function: read a complete file into a QString (without error checking)
+// Helper function: read a complete file into a QString
 static QString readFile(const char* fileName)
 {
+    QString s;
     QFile f(fileName);
-    f.open(QIODevice::ReadOnly);
-    QTextStream in(&f);
-    return in.readAll();
+    bool ok = f.open(QIODevice::ReadOnly);
+    if (ok) {
+        QTextStream in(&f);
+        s = in.readAll();
+    }
+    return s;
 }
 
 // Helper function: get a string from GL
